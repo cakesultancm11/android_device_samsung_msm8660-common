b843777cc380790c2d7d3bb6343ce470c470eb53
khusika <khusikadhamar@gmail.com>
2015-08-10 08:55:00 -0400

    msm8660: Remove deprecated egl.cfg
    see here: https://github.com/CyanogenMod/android_frameworks_native/commit/993814255fc248454368ed9fe34b4703a05eaf99
    
    Change-Id: Ib29fc8685df1de424a0b50733acb0d194b8c9e4f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -50,7 +50,6 @@ BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charg
 BOARD_HARDWARE_CLASS += device/samsung/msm8660-common/cmhw
 
 # Display
-BOARD_EGL_CFG := device/samsung/msm8660-common/configs/egl.cfg
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_DISPLAY_INSECURE_MM_HEAP := true
